c2aee8036844f8c32719cac7a5b787ed9b660540
CyrilleKolle <ngidekollecyrille@gmail.com>
2023-02-09 13:18:17 +0100

    added test points predictions

--- code/main.py
+++ code/main.py
@@ -88,10 +88,10 @@ class Statistics_ai:
         c_i = []
 
         # For every dataframe in the dataset
-        # scipy.stats.t is a continuous probability distribution in the SciPy. 
+        # scipy.stats.t is a continuous probability distribution in the SciPy.
         # It represents the t-distribution, which is used to estimate the population mean of a normally
-        # distributed population when the sample size is small and the population standard deviation is unknown. 
-        
+        # distributed population when the sample size is small and the population standard deviation is unknown.
+
         # sem() = standard error of the mean is used in place of the standard deviation since the standard deviation is not known
         for key, value in self._df_list.items():
             c_i.append(
@@ -151,15 +151,15 @@ class Statistics_ai:
             f"\nAlternatively, We could compare the result.statistic from the ttest_ind \n"
         )
         print(
-            f"Hypothesis rejected because t_statistic({result.statistic}) is greater than 0.95 which is the significance level"
+            f"Hypothesis rejected because t_statistic({result.statistic}) is greater than {norm.ppf(0.95)} which is the critical value"
             if result.statistic > norm.ppf(0.95)
-            else f"hypothesis not rejected since t_statistic({result.statistic}) is less than 0.95 which is the signifance level"
+            else f"hypothesis not rejected since t_statistic({result.statistic}) is less than {norm.ppf(0.95)} which is the critical value"
         )
 
     def mean_hypothesis_statsmodel(self, noll_df, other_df):
         alternate_data = self._df_list[other_df][self._test]
         noll_data = self._df_list[noll_df][self._test]
-        
+
         # To cancel the potential of conflicts, ttest_ind from statsmodels.stats.weightstats was imported as it b
         result = it(
             x1=noll_data.sample(10),
@@ -229,22 +229,21 @@ class Statistics_ai:
         sns.lmplot(df1, y=sub_df, x=self._test, hue="class")
 
     def prediction_interval_out_of_our_sample_space(self, df, sub_df):
-        
-        """"
-        Since my dataset is just 50 rows per class, I thought about an out of sample prediction by using 
+
+        """ "
+        Since my dataset is just 50 rows per class, I thought about an out of sample prediction by using
         statistical models to make new predictions
         """
-        
+
         df = self._df_list[df]
-        
-        
+
         # the first prediction model will be used to plot or draw the upper and lower limits
-        # of my prediction intervall 
-        points = pd.DataFrame(np.linspace(0, 5, 50), columns=[self._test])
+        # of my prediction intervall
+        points = pd.DataFrame(np.linspace(0, 5, 5), columns=[self._test])
         model = ols(f"{sub_df} ~ {self._test}", data=df).fit()
         predictions = model.get_prediction(points)
         frame = predictions.summary_frame(alpha=0.05)
-        
+
         lower = frame["obs_ci_lower"]
         upper = frame["obs_ci_upper"]
 
@@ -252,19 +251,76 @@ class Statistics_ai:
 
         sns.scatterplot(data=df, x=self._test, y=sub_df)
 
-        
         predictions1 = model.get_prediction(df[self._test])
         frame1 = predictions1.summary_frame(alpha=0.05)
         ypred = frame1["mean"]
+
+        # test points predictions
+        ypred_testpoints = model.predict(points)
+        
+        # test points plotted
+        sns.scatterplot(
+            x=points[self._test],
+            y=ypred_testpoints,
+            zorder=10,
+            color="black",
+            marker="s",
+            label="OLS Prediction test points",
+        )
+
+        print(points.head())
 
         ax.plot(df[self._test], ypred, "r", label="OLS prediction")
 
         ax.plot(points, lower, "r--", label="lower prediction limit", linewidth=0.75)
         ax.plot(points, upper, "r--", label="upper prediction limit", linewidth=0.95)
         ax.legend(loc="best")
-        plt.title('Out of sample regression prediction')
-        
+        plt.title("Out of sample regression prediction")
+
         # Use scipy stats to compute pearson correlation coefficient and display on plot
         r, p = scs.pearsonr(x=df[self._test], y=df[sub_df])
-        plt.text(.3, 0.8, 'r={:.2f}'.format(r), transform=ax.transAxes)
+        plt.text(0.3, 0.8, "r={:.2f}".format(r), transform=ax.transAxes)
+        plt.show()
+
+    def prediction_interval_in_our_sample_space(self, df, sub_df):
+        df = self._df_list[df]
+        model = ols(f"{sub_df} ~ {self._test}", data=df).fit()
+        predictions = model.get_prediction(df[self._test])
+        
+        points = pd.DataFrame(np.linspace(0, 1, 5), columns=[self._test])
+
+        frame = predictions.summary_frame(alpha=0.05)
+        ypred = frame["mean"]
+
+        order = np.argsort(df[self._test])
+        xs = np.array(df[self._test])[order]
+        # ys = np.array(frame['obs_ci_lower'])[order]
+
+        fig, ax = plt.subplots(figsize=(10, 5))
+
+        sns.scatterplot(df, x=self._test, y=sub_df)
+        ax.plot(df[self._test], ypred, "r", label="OLS prediction")
+
+        order = np.argsort(df[self._test])
+        x = np.array(df[self._test])[order]
+        lower = np.array(frame["obs_ci_lower"])[order]
+        upper = np.array(frame["obs_ci_upper"])[order]
+        
+        # prediction for the random points
+        ypred_testpoints = model.predict(points)
+        
+        # plotting of the random points
+        sns.scatterplot(
+            x=points[self._test],
+            y=ypred_testpoints,
+            zorder=10,
+            color="black",
+            marker="s",
+            label="OLS Predictions for test points",
+        )
+
+        ax.plot(xs, lower, "r--", label="lower pred lim", linewidth=0.75)
+        ax.plot(xs, upper, "r--", label="upper pred lim", linewidth=0.75)
+
+        ax.legend(loc="best")
         plt.show()
